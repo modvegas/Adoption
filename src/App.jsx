8cc8b72a4738d212c24f886d14375786a4283fce
modvegas <uokcelik@MagiClick.com>
2023-11-02 15:31:07 +0300

    Jsx refactor & eslint settings added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
